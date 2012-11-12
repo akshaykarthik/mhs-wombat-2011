06df9b06ab8eb3c371f373d19b1acdfa633bbab8
Drew S <amsorrels@gmail.com>
2012-11-12 12:42:04 -0500

    Some Updates to Server
    Server side of the chat in Lobby via TCP connection.  When you leave
    lobby, closes the tcp connection.  UDP for the game

--- server/GameServer.java
+++ server/GameServer.java
@@ -8,7 +8,7 @@ public class GameServer
 		public static void main(String[] args) throws IOException
 			{
 				ServerSocket server = null;
-				boolean inGame = true;
+				boolean inLobby = true;
 				try{										
 					server = new ServerSocket(27000);     		//Make it so that the server DatagramSocket port # can change via reading a text document
 				} 
@@ -16,11 +16,15 @@ public class GameServer
 					System.err.println("Could not listen on port: 27000");
 					System.exit(-1);
 				}
-				new GameServerThread().start();
-				while(inGame)
-					new GameServerThreadTCP(server.accept()).start();
 				
+				while(inLobby)
+					new GameServerThreadTCP(server.accept()).start();
 				server.close();
+				
+				new GameServerThread().start();
+				
+				
+				
 				
 			}
 
--- server/GameServerThreadTCP.java
+++ server/GameServerThreadTCP.java
@@ -12,8 +12,23 @@ public class GameServerThreadTCP extends Thread
 		
 		public void run() {
 			try {
-				//while()   //when you are in game, breaks on quit
 				
+				PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				
+				String input, output;
+				output = "Welcome to the Lobby";
+				out.println(output);
+				
+				while((input = in.readLine()) != null){
+					//if the client didn't send the message
+					output = in.readLine();
+					System.out.println(output);
+					out.println(output);
+					//if they exit lobby or join a game
+				}
+				out.close();
+				in.close();
 				socket.close();
 			}
 			catch(IOException ioe) {
