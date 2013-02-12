b1e8d17dd90d2c46eaab94d3640a65a84d3094d7
Drew Sorrels <amsorrels@gmail.com>
2013-02-12 13:04:17 -0500

    first .cpp w/o any c compiler

---
+++ super-racer/server/jni/GameState.cpp
@@ -0,0 +1,34 @@
+/*
+ * GameState.cpp
+ *
+ *  Created on: Feb 12, 2013
+ *      Author: Drew
+ */
+
+#include "GameState.h"
+#include <iostream>
+using namespace std;
+
+class GameState {
+
+};
+
+JNIEXPORT jint JNICALL Java_GameState_intMethod(JNIEnv *env, jobject obj) {
+
+}
+
+JNIEXPORT jstring JNICALL Java_GameState_strMethod(JNIEnv *env, jobject obj) {
+
+}
+
+JNIEXPORT void JNICALL Java_GameState_voidMethod(JNIEnv *env, jobject obj) {
+
+}
+
+JNIEXPORT void JNICALL Java_GameState_drawImage(JNIEnv *env, jobject obj) {
+
+}
+
+JNIEXPORT void JNICALL Java_GameState_Physics(JNIEnv *env, jobject obj) {
+
+}
