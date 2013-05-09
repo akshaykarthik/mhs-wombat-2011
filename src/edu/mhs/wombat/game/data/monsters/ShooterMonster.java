8fba049a66ed35a0d533a8266d163ec25aea4198
Drew Sorrels <amsorrels@gmail.com>
2013-05-08 22:18:02 -0400

    shooter monster

---
+++ src/edu/mhs/wombat/game/data/monsters/ShooterMonster.java
@@ -0,0 +1,75 @@
+package edu.mhs.wombat.game.data.monsters;
+
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.geom.Shape;
+import org.newdawn.slick.geom.Vector2f;
+import org.newdawn.slick.state.StateBasedGame;
+
+import edu.mhs.wombat.game.GameStatus;
+import edu.mhs.wombat.game.core.Entity;
+import edu.mhs.wombat.game.core.EntityState;
+import edu.mhs.wombat.game.data.player.Player;
+
+public class ShooterMonster extends Monster {
+
+	@Override
+	public EntityState getState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setState(EntityState es) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void init(GameStatus gs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(StateBasedGame game, GameStatus gs, int delta) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void render(StateBasedGame game, Graphics g) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void close(GameStatus gs) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Shape getHitBox() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void collideWith(Entity b) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void playerCollide(Player a) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Vector2f getPos() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
